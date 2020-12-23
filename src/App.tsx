d4fe9d456e88f8811c02593cfad0c71faf4312d1
oyvindsabo <oyvind.sabo@gmail.com>
2020-12-23 09:01:55 +0100

    Add prettier and increase max line length

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import Clock from './Clock';
-import { useOnce } from './utils';
-import './App.css';
+import React, { useState } from "react";
+import Clock from "./Clock";
+import { useOnce } from "./utils";
+import "./App.css";
 
 const getWakeLockSentinel = async () => {
   try {
-    const wakeLockSentinel = await navigator.wakeLock.request('screen');
-    wakeLockSentinel.addEventListener('release', () => {
-      console.log('Screen Wake Lock released:', wakeLockSentinel.released);
+    const wakeLockSentinel = await navigator.wakeLock.request("screen");
+    wakeLockSentinel.addEventListener("release", () => {
+      console.log("Screen Wake Lock released:", wakeLockSentinel.released);
     });
-    console.log('Screen Wake Lock released:', wakeLockSentinel.released);
+    console.log("Screen Wake Lock released:", wakeLockSentinel.released);
     return wakeLockSentinel;
   } catch (err) {
     console.error(`${err.name}, ${err.message}`);
@@ -17,42 +17,26 @@ const getWakeLockSentinel = async () => {
   }
 };
 
-const doReleaseWakeLock = (wakeLockSentinel: WakeLockSentinel | null) => () => {
-  if (!wakeLockSentinel) return;
-  wakeLockSentinel.release();
-};
-
-const doAddVisibilityChangeListener = (
-  handleVisibilityChange: VoidFunction
-) => {
-  document.addEventListener('visibilitychange', handleVisibilityChange);
-};
-
-const doRemoveVisibilityChangeListener = (
-  handleVisibilityChange: VoidFunction
-) => () => {
-  document.removeEventListener('visibilitychange', handleVisibilityChange);
-};
-
 const App = () => {
-  const [
-    wakeLockSentinel,
-    setWakeLockSentinel,
-  ] = useState<WakeLockSentinel | null>(null);
+  const [wakeLockSentinel, setWakeLockSentinel] = useState<WakeLockSentinel | null>(null);
 
   const handleVisibilityChange = () => {
-    if (document.visibilityState !== 'visible') return;
+    if (document.visibilityState !== "visible") return;
     getWakeLockSentinel().then(setWakeLockSentinel);
   };
 
   useOnce(() => {
     getWakeLockSentinel().then(setWakeLockSentinel);
-    return doReleaseWakeLock(wakeLockSentinel);
+    return () => {
+      wakeLockSentinel?.release();
+    };
   });
 
   useOnce(() => {
-    doAddVisibilityChangeListener(handleVisibilityChange);
-    return doRemoveVisibilityChangeListener(handleVisibilityChange);
+    document.addEventListener("visibilitychange", handleVisibilityChange);
+    return () => {
+      document.removeEventListener("visibilitychange", handleVisibilityChange);
+    };
   });
 
   return <Clock />;
