87194872e0919ebae7ec101a9ecf3dd3429189f7
oyvindsabo <oyvind.sabo@gmail.com>
2020-12-27 15:29:14 +0100

    Make the time embossed into the page

--- src/Clock.tsx
+++ src/Clock.tsx
@@ -2,11 +2,23 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import "./App.css";
 
-const Time = styled.div`
-  color: white;
-  font-size: min(25vw, 100vh);
+const Wrapper = styled.div`
   text-align: center;
-  line-height: 100vh;
+`;
+
+const Time = styled.span`
+  margin-top: 50vh;
+  transform: translate(0, -50%);
+  color: #00a6d7;
+  background: #0071b4;
+  display: inline-block;
+  font-size: min(15vw, 75vh);
+  height: min(15vw, 75vh);
+  line-height: min(15vw, 75vh);
+  font-weight: 100;
+  font-family: monospace;
+  box-shadow: inset 5px 5px 10px rgba(0, 0, 0, 0.2);
+  text-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
 `;
 
 const getTimeString = () => {
@@ -23,7 +35,11 @@ const Clock = () => {
     setTimeout(initializeTime, 1000);
   };
   useEffect(initializeTime);
-  return <Time>{timeString}</Time>;
+  return (
+    <Wrapper>
+      <Time>{timeString}</Time>
+    </Wrapper>
+  );
 };
 
 export default Clock;
